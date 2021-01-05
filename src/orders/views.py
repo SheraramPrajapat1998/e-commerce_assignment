f6670a8bc451eda3e0066838aa12e5cf429eaee0
SheraramPrajapat1998 <sheraramprajapat1998@gmail.com>
2021-01-06 00:44:12 +0530

    added order detail and export to csv functionality for orders admin section

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from cart.cart import Cart
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404, render
 
-from .models import OrderItem
 from .forms import OrderCreateForm
 from .mail import order_created
+from .models import Order, OrderItem
 
-from cart.cart import Cart
 
 def order_create(request):
   cart = Cart(request)
@@ -27,3 +28,8 @@ def order_create(request):
     form = OrderCreateForm()
   return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
 
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+  order = get_object_or_404(Order, id=order_id)
+  return render(request, 'admin/orders/order/detail.html', {'order': order})
