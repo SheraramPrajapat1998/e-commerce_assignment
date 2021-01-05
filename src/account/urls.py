ed1bf3be731f43f9cddfa97a9462d55d453362f2
SheraramPrajapat1998 <sheraramprajapat1998@gmail.com>
2021-01-05 09:31:41 +0530

    registeration fucntionality added

--- src/account/urls.py
+++ src/account/urls.py
@@ -7,7 +7,8 @@ from .import views
 urlpatterns = [
   path('login/', auth_views.LoginView.as_view(), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-  
+
+  path('register/', views.register, name='register'),  
   path('', views.dashboard, name='dashboard'),
 
   # change password urls
--- src/account/views.py
+++ src/account/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
 from django.contrib.auth.decorators import login_required
+
+from .forms import UserRegistrationForm
 
 def dashboard(reqeust):
   return render(request, 'account/dashboard.html')
   
+def register(request):
+  user_form = UserRegistrationForm()
+  if request.method == 'POST':
+    user_form = UserRegistrationForm(data=request.POST)
+    if user_form.is_valid():
+      new_user = user_form.save(commit=False)
+      # save encrypted password instead of raw password
+      new_user.set_password(user_form.cleaned_data['password'])
+      new_user.save()
+      return render(request, 'account/register_done.html', {'new_user':new_user})
+  return render(request, 'account/register.html', {'user_form':user_form})
